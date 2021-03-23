3794990390bf8b4a3836ea559ecdc16315abac8e
reii-code <80747218+reii-code@users.noreply.github.com>
2021-03-23 14:27:41 +0700

    Add files via upload

---
+++ Pertemuan 3/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+
+public class test {
+    public static void main(String[] args) {
+        GamePlayer demo = new GamePlayer();
+        demo.setPosition(30,40);
+        demo.Run(60);
+        demo.Run();
+    }
+    
+}
